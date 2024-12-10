1e5976abc517a8ffb7ada0ab76d3f7388d3a761e
laribox <laribox2014@gmail.com>
2024-12-10 13:58:10 -0800

    fix lint

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,29 @@
 // routes/index.js
 
-import FilesController from "../controllers/FilesController";
+import FilesController from '../controllers/FilesController';
 
-const express = require("express");
-const AppController = require("../controllers/AppController");
-const UsersController = require("../controllers/UsersController");
-const AuthController = require("../controllers/AuthController");
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 const router = express.Router();
 
 // Define routes
-router.get("/status", AppController.getStatus);
-router.get("/stats", AppController.getStats);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-router.post("/users", UsersController.postNew);
-router.get("/users/me", UsersController.getMe);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
-router.get("/connect", AuthController.getConnect);
-router.get("/disconnect", AuthController.getDisconnect);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
-router.post("/files", FilesController.postUpload);
-router.get("/files/:id", FilesController.getShow);
-router.get("/files", FilesController.getIndex);
-router.put("/files/:id/publish", FilesController.putPublish);
-router.put("/files/:id/unpublish", FilesController.putUnpublish);
-router.get("/files/:id/data", FilesController.getFile);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
